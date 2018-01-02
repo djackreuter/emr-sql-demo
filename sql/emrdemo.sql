4c6cc662c8ec8c7d9f9a04b3da281c857aa90836
Jack Reuter <djreuter45@gmail.com>
2018-01-02 10:10:43 -0700

    added users table

--- sql/emrdemo.sql
+++ sql/emrdemo.sql
@@ -8,3 +8,13 @@ CREATE USER 'jimmy'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON emr_sql_demo.* TO 'jimmy'@'localhost';
 
 -- create users table
+CREATE TABLE users (
+  id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+  user_name VARCHAR(50) NOT NULL,
+  user_gender TINYINT UNSIGNED,
+  user_email VARCHAR(128) NOT NULL,
+  user_hash CHAR(128),
+  user_salt CHAR(64),
+  UNIQUE (user_email),
+  PRIMARY KEY (id)
+);
