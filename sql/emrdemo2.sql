c6dd73daf34b1ad324b76e59df006f80ced47105
Jack Reuter <djreuter45@gmail.com>
2018-01-02 15:38:00 -0700

    added posts and comments table

--- sql/emrdemo2.sql
+++ sql/emrdemo2.sql
@@ -19,3 +19,23 @@ CREATE TABLE users (
   user_hash CHAR(128),
   user_salt CHAR(64)
 );
+
+-- create posts table
+CREATE TABLE posts (
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  user_id INT NOT NULL REFERENCES users (id),
+  post_title VARCHAR(64),
+  post_content VARCHAR(1000) NOT NULL,
+  post_date TIMESTAMP(6) NOT NULL
+);
+-- add index
+CREATE INDEX user_id ON posts (user_id);
+
+-- create comments table
+CREATE TABLE comments (
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  user_id INT NOT NULL,
+  comment_body VARCHAR(500) NOT NULL,
+  comment_date TIMESTAMP(6) NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id)
+);
